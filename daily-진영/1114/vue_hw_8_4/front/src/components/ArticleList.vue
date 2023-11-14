605e29eb6203509dc8eee67e96e35a8bbc635aa5
김진영 <kimyoung100@naver.com>
2023-11-14 17:03:41 +0900

    1114

---
+++ daily-진영/1114/vue_hw_8_4/front/src/components/ArticleList.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="goCreate">게시글 생성</button>
+  <div v-for="article in store.articles" 
+  :key="article.id"
+  :article="article">
+    <p>{{ article.id }}번 게시글</p>
+    <p>제목 : {{ article.title }}</p>
+    <p>내용 : {{ article.content }}</p>
+    <hr>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router'
+import { useArticleStore } from '@/stores/articles' 
+const store = useArticleStore()
+
+onMounted(() => {
+  store.getArticles()
+})
+
+const router = useRouter()
+
+const goCreate = function() {
+  router.push({
+    name:'create'
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
